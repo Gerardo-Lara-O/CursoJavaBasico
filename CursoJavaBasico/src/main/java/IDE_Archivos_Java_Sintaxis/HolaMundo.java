4ee684ec47b0b3a9ee4da6ec9f1cbe490cd1274b
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2023-03-15 03:21:52 -0600

    javadoc

--- CursoJavaBasico/src/main/java/IDE_Archivos_Java_Sintaxis/HolaMundo.java
+++ CursoJavaBasico/src/main/java/IDE_Archivos_Java_Sintaxis/HolaMundo.java
@@ -1,7 +1,15 @@
 package IDE_Archivos_Java_Sintaxis;
 
+/**
+ * Clase de prueba para crear un programa basico en java
+ */
 public class HolaMundo {
+    /**
+     * Punto de entrada a la aplicacion java
+     * Permite ejecutar la aplicacion
+     * @param args - argumentos de entrada para la aplicacion
+     */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hello World");
     }
 }
