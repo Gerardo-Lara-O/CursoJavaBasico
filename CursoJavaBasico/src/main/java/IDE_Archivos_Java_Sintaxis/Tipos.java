1e2c9ed6f1218d65280c726411c5746c70ed6d45
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2023-03-15 21:05:08 -0600

    Tipos de variables

---
+++ CursoJavaBasico/src/main/java/IDE_Archivos_Java_Sintaxis/Tipos.java
@@ -0,0 +1,31 @@
+package IDE_Archivos_Java_Sintaxis;
+
+public class Tipos {
+    public static void main(String[] args) {
+        /*
+        Tipos de Datos
+        estructura:
+        especificar tipo de dato | identificador | operador de asignacion
+         */
+
+        //1- numericos
+        //1.1 - enteros
+        byte variable1 = 5;
+        short variable2 = 10;
+        int variable3 = 30;
+        long variable4 = 100;
+
+        //1.2 - decimales
+        float variable5 = 5.5f;
+        double variable6 = 10.5d;
+
+        //2 - booleanos
+        boolean variable7 = false;
+        boolean variable8 = true;
+
+        //3 - texto
+        char variable9 = 'a';
+        String variable10 = "Lorem";
+
+    }
+}
