e5a04e51bb334f42e0f7aaf8a69c9d53718509d4
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2023-03-17 02:35:47 -0600

    Funciones

--- CursoJavaBasico/src/main/java/Funciones/Funciones.java
+++ CursoJavaBasico/src/main/java/Funciones/Funciones.java
@@ -10,12 +10,30 @@ public class Funciones {
 //        showMenu();
 
         //opcion2: funcion sin parametros y con tipo de retorno
-        String menu = getMenu();
-        System.out.println(menu);
+//        String menu = getMenu();
+//        System.out.println(menu);
+//        System.out.println(getMenu());
+//
+//        double price = getPrice();
+//        System.out.println(price);
+
+        //Opcion3 : funcion con parametros y sin tipo de retorno
+        imprimirSaludoBuenosDias("Gerardo");
+
+        //opcion 4: funcion con parametros y con tipo de retorno
+        String nombre = "Gerardo";
+        String apellido = "Lara";
+        String saludo = obtenerSaludo(nombre,apellido);
+        System.out.println(saludo);
+
 
 
     }
+
+
+
     // identificador | parentesis | llaves
+    //opcion 1
     static void showMenu(){
         System.out.println("Bienvenido al E-comerce de zapatillas:");
         System.out.println("1 - ver zapatillas");
@@ -23,7 +41,27 @@ public class Funciones {
         System.out.println("3 - salir");
     }
 
+    //opcion 2
     static String getMenu(){
+        System.out.println("Imprimiendo texto prueba");
         return "Bienvenido al E-comerce de zapatillas: \n 1 - ver zapatillas";
     }
+
+
+    //opcion 3
+    static double getPrice() {
+        return 100.99;
+    }
+    static void imprimirSaludoBuenosDias(String name){
+        System.out.println("Buenos dias " + name);
+    }
+
+    //opcion 4
+    static String obtenerSaludo(String nombre, String apellidos){
+        return "Buenas tardes "+ nombre + " " + apellidos;
+    }
+
+
+
+
 }
---
+++ CursoJavaBasico/src/main/java/Funciones/Funciones2.java
@@ -0,0 +1,13 @@
+package Funciones;
+
+public class Funciones2 {
+    public static void main(String[] args) {
+
+        int resultado1 = suma(50,50);
+        System.out.println("El resultado es: " + resultado1);
+
+    }
+    static int suma(int numero1 , int numero2){
+        return numero1 + numero2;
+    }
+}
